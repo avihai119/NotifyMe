332e923eafa08ef79e4c83e2a9a0bb17a83cf84b
avihai119 <avihi119@gmail.com>
2025-11-24 14:33:32 +0200

    restyled mainwindow.py to a horizontal layout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication
-from ui.main_window import MainWindow
+from ui.mainwindow import MainWindow
 import sys
 
 def main():
--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -1,24 +1,43 @@
-import getpass
 import sys
+import getpass
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
-class MyWidget (QtWidgets.QWidget):
+class MainWindow (QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-
-        self.user_name = getpass.getuser()
-        self.hello = f"Hello {self.user_name}"
-        self.button = QtWidgets.QPushButton("Get Started")
-        self.text = QtWidgets.QLabel(self.hello,
-                                     alignment=QtCore.Qt.AlignCenter)
         
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        self.setWindowTitle("NotifyMe")
+        
+        self.user_name = getpass.getuser()
+        self.user_text = f"Hello {self.user_name}, welcome to NotifyMe!"
+        
+        self.text = QtWidgets.QLabel(f"Hello {self.user_name}", alignment=QtCore.Qt.AlignCenter)
+        self.button = QtWidgets.QPushButton("Get Started")
+        self.start_button = QtWidgets.QPushButton("Start Timer")
+        self.stop_button = QtWidgets.QPushButton("Stop Timer")
+        self.settings_button = QtWidgets.QPushButton("Settings")
 
+        self.top_buttons_layout = QtWidgets.QHBoxLayout()
+        self.top_buttons_layout.addWidget(self.button)
+        self.top_buttons_layout.addWidget(self.start_button)
+        
+        self.bottom_buttons_layout = QtWidgets.QHBoxLayout()
+        self.bottom_buttons_layout.addWidget(self.stop_button)
+        self.bottom_buttons_layout.addWidget(self.settings_button)
+
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        
+        self.main_layout.addWidget(self.text)
+        self.main_layout.addLayout(self.top_buttons_layout)
+        self.main_layout.addLayout(self.bottom_buttons_layout)
+        
         self.button.clicked.connect(self.magic)
 
     @QtCore.Slot()
     def magic(self):
-        self.text.setText(random.choice(self.hello))
+        if self.user_text:
+            new_char = random.choice(self.user_text)
+            self.text.setText(f"Random character: **{new_char}**")
+        else:
+            self.text.setText("Text is empty.")
